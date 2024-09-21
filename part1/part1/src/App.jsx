e1fde6e4c6c5d08f44582a9a4224b1bea2de844f
Vikas Sharma <vkassharma19492@gmail.com>
2024-09-21 19:16:25 +0530

    step1.1

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,7 +1,15 @@
 const App = () => {
+  // console.log('Hello from component');
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+  console.log(now, a+b);
   return (
     <div>
-      <p>Hello World</p>
+      <p>Hello World, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a+b}
+      </p>
     </div>
   )
 };
