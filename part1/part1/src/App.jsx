a41764d2aec0774a8df51158f5c42aa51987c813
Vikas Sharma <vkassharma19492@gmail.com>
2024-09-21 19:25:31 +0530

    step1.2

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,15 +1,34 @@
+// const App = () => {
+//   // console.log('Hello from component');
+//   const now = new Date();
+//   const a = 10;
+//   const b = 20;
+//   console.log(now, a+b);
+//   return (
+//     <div>
+//       <p>Hello World, it is {now.toString()}</p>
+//       <p>
+//         {a} plus {b} is {a+b}
+//       </p>
+//     </div>
+//   )
+// };
+
+// export default App;
+
+const Hello = () => {
+  return (
+    <p>Hello world</p>
+  )
+};
+
 const App = () => {
-  // console.log('Hello from component');
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-  console.log(now, a+b);
   return (
     <div>
-      <p>Hello World, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a+b}
-      </p>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
   )
 };
